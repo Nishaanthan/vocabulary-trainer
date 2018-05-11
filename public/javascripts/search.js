744eed2d61029cded2c12635d139fd991253b300
Nisanthan <nisanthan@email.com>
2018-05-11 10:55:55 +0530

    added search.js

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,3 +1,3 @@
 // translate button to a variable
 translateButton = document.getElementById("translate");
-translateButton.onclick = ()=>console.log("this button is clicked"); 
+translateButton.onclick = ()=>console.log("this button is clicked");
