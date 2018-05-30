24035be56cd7b1b6fa005fb0fd8e13c925211461
Nisanthan <nisanthan@email.com>
2018-05-30 15:25:10 +0530

    added .env file to store environment variables

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to Vocabulary trainer app' });
 });
-router.post('/',function(req, res, next){
-  console.log(req.body.original);
-  console.log(req.body.translated);
+router.post('/',function(req, res){
+  
 })
 
 
