4b35fdbf0da78567d421b4f3a4cd2f10dcda2e02
Nisanthan <nisanthan@email.com>
2018-05-30 14:38:16 +0530

    added body parser package

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to Vocabulary trainer app' });
 });
+router.post('/',function(req, res, next){
+  console.log(req.body.original);
+  console.log(req.body.translated);
+})
 
 
 module.exports = router;
